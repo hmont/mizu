41fd92dfb7f4089cfe2ab7999441ef67b4c13cd6
Henry <hi@henry.moe>
2025-05-02 13:26:47 -0400

    s

--- src/web/api/__init__.py
+++ src/web/api/__init__.py
@@ -4,8 +4,7 @@ from .register import router as register_router
 from .auth import router as auth_router
 
 api_router = APIRouter(
-    prefix="/api"
-)
+    prefix="/api")
 
 api_router.include_router(register_router)
 api_router.include_router(auth_router)
